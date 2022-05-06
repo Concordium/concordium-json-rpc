4cfd90301760c050ff2bb7b9ce866c8cb4f38448
Jakob Ørhøj <jo@concordium.com>
2022-05-06 12:51:59 +0200

    Add support for getNextAccountNonce

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Concordium JSON-RPC proxy server
 
-The Concordium JSON-RPC proxy serves is a small server that acts as a wrapper for the gRPC interface exposed by the Concordium node.
+The Concordium JSON-RPC proxy server is a small server that acts as a wrapper for the gRPC interface exposed by the Concordium node.
 
 # Build
 To build the project run:
@@ -11,7 +11,11 @@ yarn build
 # Run server
 When the project has been built you can run it locally with node with the following command:
 ```
-node dist/index.js
+yarn start --port jsonRpcServerPort --nodeAddress nodeIpAddress --nodePort nodePort --nodeTimeout nodeTimeout
+```
+An example of running the server with a locally running Concordium node would be:
+```
+yarn start --port 9095 --nodeAddress 127.0.0.1 --nodePort 10001 --nodeTimeout 5000
 ```
 
 # Linting
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
+import { credentials, Metadata } from '@grpc/grpc-js';
 import express from 'express';
-import jayson, { JSONRPCCallbackTypePlain } from 'jayson';
+import jayson from 'jayson';
 import minimist from 'minimist';
+import NodeClient from './client';
+import getJsonRpcMethods from './methods';
 
+// Parse parameters that defines how to set up the server.
 const argv = minimist(process.argv.slice(2));
 const jsonRpcPort = Number(argv.port);
+const nodeAddress = argv.nodeAddress;
+const nodePort = Number(argv.nodePort);
+const nodeTimeout = Number(argv.nodeTimeout);
+
+const metadata = new Metadata();
+metadata.add('authentication', 'rpcadmin');
+const insecureCredentials = credentials.createInsecure();
+const nodeClient = new NodeClient(
+    nodeAddress,
+    nodePort,
+    insecureCredentials,
+    metadata,
+    nodeTimeout
+);
+const server = new jayson.Server(getJsonRpcMethods(nodeClient));
 
 const app = express();
-
-const server = new jayson.Server({
-    add: function (
-        params: { foo: number; bar: number },
-        callback: JSONRPCCallbackTypePlain
-    ) {
-        callback(null, params.foo + params.bar);
-    },
-});
-
 app.use(express.json());
 app.post('/json-rpc', server.middleware());
 app.listen(jsonRpcPort);
