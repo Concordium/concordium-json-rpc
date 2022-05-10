593a56a7413ab100e813fbfc07d056123f656a7b
orhoj <jo@concordium.com>
2022-05-10 14:54:34 +0200

    Add simple logging of requests and errors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import jayson from 'jayson';
 import minimist from 'minimist';
 import NodeClient from './client';
 import getJsonRpcMethods from './methods';
+import logger from './logger';
+import { v4 as uuidv4 } from 'uuid';
 
 // Parse parameters that defines how to set up the server.
 const argv = minimist(process.argv.slice(2));
@@ -26,9 +28,37 @@ const server = new jayson.Server(getJsonRpcMethods(nodeClient));
 
 const app = express();
 app.use(express.json());
-app.post('/json-rpc', server.middleware());
+app.post('/json-rpc', (req, res, next) => {
+    const correlationId = uuidv4();
+    const request = req.body;
+    logger.info('Received a request', { correlationId });
+
+    server.call(request, (error, response) => {
+        if (error) {
+            logger.error('Failed request', { error, correlationId, request });
+
+            // If err is an Error, then it is not a JSON-RPC error.
+            if (error instanceof Error) {
+                return next(error);
+            }
+
+            res.status(400);
+            res.send(error);
+            return;
+        }
+
+        if (response) {
+            logger.info('Successful request', { correlationId });
+            return res.send(response);
+        } else {
+            res.status(204);
+            res.send('');
+        }
+    });
+});
+
 app.listen(jsonRpcPort);
 
-console.log(
+logger.info(
     'Concordium JSON-RPC server is now listening on port ' + jsonRpcPort
-);
+, { nodeAddress, nodePort, nodeTimeout });
---
+++ src/logger.ts
@@ -0,0 +1,20 @@
+import winston from 'winston';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [new winston.transports.File({ filename: 'combined.log' })],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        })
+    );
+}
+
+export default logger;
