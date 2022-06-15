20a22e2b666ef30c78f2d2ad78346b5e9b44be80
Hjort <shj@concordium.com>
2022-06-15 08:31:14 +0200

    make JSONbig use native bigints

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ import JSONbig from 'json-bigint';
 // DANGEROUSLY override JSON prototype methods to handle uint64 values (bigint)
 // To avoid this we would have to write our own parser/serializer for the express
 // server.
-JSON.parse = JSONbig.parse;
-JSON.stringify = JSONbig.stringify;
+JSON.parse = JSONbig({ useNativeBigInt: true }).parse;
+JSON.stringify = JSONbig({ useNativeBigInt: true }).stringify;
 
 // Parse parameters that defines how to set up the server.
 const argv = minimist(process.argv.slice(2));
